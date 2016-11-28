92f926be5c0ce8f58b5379752703776235407b18
vieck <vieck@purdue.edu>
2016-11-27 20:32:57 -0500

    Login UI done

--- app/login/login.js
+++ app/login/login.js
@@ -2,13 +2,20 @@
 
 angular.module('myApp.login', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/login', {
-    templateUrl: 'app/login/login.html',
-    controller: 'LoginCtrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/login', {
+            templateUrl: 'app/login/login.html',
+            controller: 'LoginCtrl'
+        });
+    }])
 
-.controller('LoginCtrl', [function() {
+    .controller('LoginCtrl', function ($scope, $location) {
+        $scope.login = {
+            user: '',
+            password: ''
+        };
 
-}]);
+        $scope.authenticate = function () {
+
+        }
+    });
