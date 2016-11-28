93b67ee778eccfb0643b263c38f9539dccb9cbe4
vieck <vieck@purdue.edu>
2016-11-27 22:15:11 -0500

    Added HTTP callbacks for login and registration pages

--- app/register/register.js
+++ app/register/register.js
@@ -3,6 +3,9 @@
 angular.module('myApp.register', ['ngRoute'])
 
     .config(['$routeProvider', function ($routeProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        $httpProvider.defaults.headers.common["Accept"] = "application/json";
+        $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
         $routeProvider.when('/register', {
             templateUrl: 'app/register/register.html',
             controller: 'RegisterCtrl'
@@ -17,7 +20,22 @@ angular.module('myApp.register', ['ngRoute'])
             password: ''
         };
 
-        $scope.create = function () {
+        $scope.base_url = "https://www.google.com";
 
+        $scope.create = function () {
+            // POST request for creating a user
+            $http({
+                method: 'POST',
+                url: $scope.base_url + '/some-endpoint',
+                withCredentials: true,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                data: $.param({first: $scope.register.first, last: $scope.register.last, name: $scope.register.user, password: $scope.register.password})
+            }).then(function successCallback(response) {
+                //All data returned will be under response.data
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+
+            });
         }
     });
