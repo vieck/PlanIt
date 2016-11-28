a7f30b98a8dffae2326573cbc903f0405c077f0d
vieck <vieck@purdue.edu>
2016-11-27 20:06:13 -0500

    Material styles working

--- app/app.js
+++ app/app.js
@@ -2,13 +2,13 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.login',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+    'ngRoute',
+    'ngMaterial',
+    'myApp.login',
+    'myApp.view2',
+    'myApp.version'
+]).config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/login'});
+    $routeProvider.otherwise({redirectTo: '/login'});
 }]);
